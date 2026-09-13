5faefe5ae0800b555845f3ed120ce7ca3a52a49e
mauricioncabral <cabralm547@gmail.com>
2026-09-13 18:05:10 -0300

    Update merenda.js

--- js/merenda.js
+++ js/merenda.js
@@ -1,3 +1,7 @@
+if (!localStorage.getItem('usuario_nome')) {
+          alert('Acesso negado! Faça login primeiro.');
+          window.location.href = 'login.html';
+      }
 let qtd = 0;
 const qtdText = document.getElementById('quantidade');
 
