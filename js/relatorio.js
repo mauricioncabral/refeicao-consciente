925938e021c5bf8f834d4cf2825cd12538c50fef
mauricioncabral <cabralm547@gmail.com>
2026-09-13 18:04:25 -0300

    Update relatorio.js

--- js/relatorio.js
+++ js/relatorio.js
@@ -1,3 +1,7 @@
+ if (!localStorage.getItem('usuario_nome')) {
+          alert('Acesso negado! Faça login primeiro.');
+          window.location.href = 'login.html';
+      }
 const API_URL = 'api/api.php';
 
 async function buscarRelatorioMensal() {
